5b9e044dc450f935d462982d2a5ec9b78177fca3
Panguluri Tejaswi <dadgi.rl1008@gmail.com>
2026-03-03 22:37:43 +0530

    Create create_tables.sql

---
+++ schema/create_tables.sql
@@ -0,0 +1,28 @@
+-- Create Students Table
+CREATE TABLE Students (
+    student_id INT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT CHECK (age >= 16),
+    department VARCHAR(50) NOT NULL
+);
+
+-- Create Courses Table
+CREATE TABLE Courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(100) NOT NULL,
+    credits INT CHECK (credits > 0)
+);
+
+-- Create Enrollment Table (Many-to-Many Relationship)
+CREATE TABLE Enrollment (
+    enrollment_id INT PRIMARY KEY,
+    student_id INT NOT NULL,
+    course_id INT NOT NULL,
+    enrollment_date DATE,
+    
+    FOREIGN KEY (student_id) REFERENCES Students(student_id),
+    FOREIGN KEY (course_id) REFERENCES Courses(course_id)
+);
+
+
+Created Students, Courses and Enrollment tables with constraints
